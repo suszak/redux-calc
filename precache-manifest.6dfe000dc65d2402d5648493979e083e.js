d516b9d5352cda01e68de68e1a61f0dd703cc391
suszak <matys.1996@wp.pl>
2020-07-30 13:03:06 +0200

    Updates

---
+++ precache-manifest.6dfe000dc65d2402d5648493979e083e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7904ead7aa094a48f3cf6ce54c8541d2",
+    "url": "/redux-calc/index.html"
+  },
+  {
+    "revision": "0a2cab4807a1347f831d",
+    "url": "/redux-calc/static/css/main.8e8a0e98.chunk.css"
+  },
+  {
+    "revision": "58285d4fbdaa5c6c2056",
+    "url": "/redux-calc/static/js/2.3416ae55.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-calc/static/js/2.3416ae55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a2cab4807a1347f831d",
+    "url": "/redux-calc/static/js/main.8b47d3d9.chunk.js"
+  },
+  {
+    "revision": "5c2843a5fa53e451ad45",
+    "url": "/redux-calc/static/js/runtime-main.d6e9c782.js"
+  }
+]);
